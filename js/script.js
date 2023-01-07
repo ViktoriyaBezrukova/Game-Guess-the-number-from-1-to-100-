134a5bbf839fc732a40a9e5c7f06fd2d3338dc6e
Viktoriya Bezruckova <stylchik02@gmail.com>
2023-01-07 16:45:12 +0300

    Задание второго урока

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 35000;
+let income = "Таксую";
+let addExpenses = "Такси, Поход в кино, Маникюр, Интернет, ЖКХ";
+let deposit = true;
+let mission = 150000;
+let period = 10;
+let budgetDay = money / 30
 
-alert('Всплывающее сообщение!')
+addExpenses = addExpenses.toLowerCase();
 
-console.log("Консоль")
+
+
+
+console.log(typeof money)
+console.log(typeof income)
+console.log(typeof deposit)
+console.log(addExpenses.length)
+console.log("Период равен " + period + " месяцев")
+console.log("Цель заработать " + mission + " рублей")
+console.log(Array.from(addExpenses))
+console.log(budgetDay)
