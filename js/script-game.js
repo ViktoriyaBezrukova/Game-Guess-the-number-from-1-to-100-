e945e82fac31a8561907961d44c280cae0d6ceea
Viktoriya Bezruckova <stylchik02@gmail.com>
2023-01-12 13:10:54 +0300

    Дописала код чтобы принимал только числа

--- js/script-game.js
+++ js/script-game.js
@@ -4,12 +4,16 @@ function randomNumber() {
     for(let quesses = 1; quesses <= 10; quesses++){
         let result = +prompt("Попытка номер" + quesses + " , Введите число ")
         console.log(number)
+        console.log(result)
         if(result == number) {
             return alert("Вы угадали число! Попыток: " + quesses + "Число: " + number)
         } else if(result < number) {
             alert("Ваше число " + result + " меньше загаданного. Попыток: " + quesses )        
         } else if(result > number) {
             alert("Ваше число " + result + " больше загаданного. Попыток: " + quesses)
+        } else if(isNaN(parseFloat(result))){
+            quesses = quesses - 1;
+            alert("Введите число!")
         } else if(quesses = 10){
             return alert("Вы не угадали число! Число :" + number)
         }
