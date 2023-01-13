27e5de27b1db86755c6daa54d67e66aa598e8e60
Viktoriya Bezruckova <stylchik02@gmail.com>
2023-01-13 13:03:07 +0300

    Сделала игру 'угадай число' с попытками

--- js/script-game.js
+++ js/script-game.js
@@ -1,24 +1,32 @@
-function randomNumber() {
-    let number = Math.floor(Math.random()*100);
-    alert("Угадайте число от 1 до 100")
-    for(let quesses = 1; quesses <= 10; quesses++){
-        let result = +prompt("Попытка номер" + quesses + " , Введите число ")
-        console.log(number)
-        console.log(result)
-        if(result == number) {
-            return alert("Вы угадали число! Попыток: " + quesses + "Число: " + number)
-        } else if(result < number) {
-            alert("Ваше число " + result + " меньше загаданного. Попыток: " + quesses )        
-        } else if(result > number) {
-            alert("Ваше число " + result + " больше загаданного. Попыток: " + quesses)
-        } else if(isNaN(parseFloat(result))){
-            quesses = quesses - 1;
-            alert("Введите число!")
-        } else if(quesses = 10){
-            return alert("Вы не угадали число! Число :" + number)
-        }
-        
+alert("Угадайте число от 1 до 100")
+let number = Math.floor(Math.random()*100);
+quesses = 0;
+
+function randomNumber() {   
+    let result = prompt("Введите число ")   
+    if(result == number) {
+        return alert("Вы угадали число! Попыток: " + quesses + "Число: " + number)
+    } else if(result < number && result != 0) {
+        alert("Ваше число " + result + " меньше загаданного. Попыток: " + quesses )   
+        quesses++;
+    } else if(result > number) {
+        alert("Ваше число " + result + " больше загаданного. Попыток: " + quesses)
+        quesses++;
+    } else if(isNaN(parseFloat(result)) || result === ""){
+        quesses = quesses - 1;
+        alert("Введите число!")
+    } else if(result === null){
+        return alert("Игра окончена! Вы нажали отмена")
     }
-    return alert("Вы не угадали число! Число :" + number)
+
+    if(quesses < 10) {
+        randomNumber();
+        
+    } else {
+        return alert("Вы не угадали число")
+    }
+    console.log(result)
 }
 randomNumber()
+console.log(typeof(prompt))
+console.log(number)
