6a87a4aeeab4af869da74ed78224a2865520bd6c
ViktoriyaBezrukova <109919216+ViktoriyaBezrukova@users.noreply.github.com>
2023-01-13 13:29:03 +0300

    Добавила изначально 10 попыток

--- js/script-game.js
+++ js/script-game.js
@@ -1,6 +1,6 @@
 alert("Угадайте число от 1 до 100")
 let number = Math.floor(Math.random()*100);
-quesses = 0;
+quesses = 10;
 
 function randomNumber() {   
     let result = +prompt("Введите число ")   
@@ -10,23 +10,23 @@ function randomNumber() {
         if(a === null){
             return alert("Игра окончена! Вы нажали отмена")
         } else{
-            quesses = 0;
+            quesses = 10;
             randomNumber()
         }
     } else if(result < number && result != 0) {
         alert("Ваше число " + result + " меньше загаданного. Попыток: " + quesses )   
-        quesses++;
+        quesses--;
     } else if(result > number) {
         alert("Ваше число " + result + " больше загаданного. Попыток: " + quesses)
-        quesses++;
+        quesses--;
     } else if(isNaN(parseFloat(result)) || result === ""){
-        quesses = quesses - 1;
+        quesses = quesses + 1;
         alert("Введите число!")
     } else if(result === 0){
         return alert("Игра окончена! Вы нажали отмена")
     }
 
-    if(quesses < 10) {
+    if(quesses > 0) {
         randomNumber();
         
     } else {
@@ -35,7 +35,7 @@ function randomNumber() {
         if(a === null){
             return alert("Игра окончена! Вы нажали отмена")
         } else{
-            quesses = 0;
+            quesses = 10;
             randomNumber()
         }
     }
