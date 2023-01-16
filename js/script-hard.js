33a9287e76db58a2d90bfb090b865bc2c9c1e1fe
ViktoriyaBezrukova <109919216+ViktoriyaBezrukova@users.noreply.github.com>
2023-01-16 13:00:54 +0300

    Delete script-hard.js

--- js/script-hard.js
+++
@@ -1,9 +0,0 @@
-let arr = ["238" , "2565", "3462", "5547", "2464", "4783", "2357"];
-
-
-for(let i = 0; i < arr.length; i++) {
-    if (String(arr[i])[0] == '2' || String(arr[i])[0] == '4' ) {
-    console.log(arr[i]);
-        }
-}
-
