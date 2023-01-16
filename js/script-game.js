16605bb70a7b9e095502b70f6da01c9e379d91fa
ViktoriyaBezrukova <109919216+ViktoriyaBezrukova@users.noreply.github.com>
2023-01-16 12:43:15 +0300

    Задание урока 6. Игра

--- js/script-game.js
+++ js/script-game.js
@@ -1,16 +1,19 @@
 alert("Угадайте число от 1 до 100")
-let number = Math.floor(Math.random()*100);
-quesses = 10;
 
-function randomNumber() {   
+
+
+quesses = 10;
+let number = Math.floor(Math.random()*100);
+function randomNumber() { 
     let result = +prompt("Введите число ")   
     if(result == number) {
         alert("Вы угадали число! Попыток: " + quesses + "Число: " + number)
         let a = prompt("Хотели бы сыграть еще?")
         if(a === null){
-            return alert("Игра окончена! Вы нажали отмена")
+            return alert("Игра окончена!")
         } else{
             quesses = 10;
+            number = Math.floor(Math.random()*100 - 1 + 1);
             randomNumber()
         }
     } else if(result < number && result != 0) {
@@ -19,7 +22,7 @@ function randomNumber() {
     } else if(result > number) {
         alert("Ваше число " + result + " больше загаданного. Попыток: " + quesses)
         quesses--;
-    } else if(isNaN(parseFloat(result)) || result === ""){
+    } else if(isNaN(parseFloat(result)) || result === " "){
         quesses = quesses + 1;
         alert("Введите число!")
     } else if(result === 0){
@@ -33,14 +36,16 @@ function randomNumber() {
         alert("Попытки закончились, вы не угадали число.")
         let a = prompt("Хотели бы сыграть еще?")
         if(a === null){
-            return alert("Игра окончена! Вы нажали отмена")
+            return alert("Игра окончена!")
         } else{
             quesses = 10;
+            number = Math.floor(Math.random()*100 - 1 + 1);
             randomNumber()
         }
     }
     console.log(result)
+    console.log(number)
 }
 randomNumber()
+
 console.log(typeof(prompt))
-console.log(number)
